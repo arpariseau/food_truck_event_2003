77c06fe177214a56eb9733df32eda458df64c0ef
Alex Pariseau <arpariseau@gmail.com>
2020-04-15 10:09:30 -0600

    Created item class, w/ tests

---
+++ test/event_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/event'
+
+class EventTest < MiniTest::Test
+
+end
---
+++ test/item_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+
+class ItemTest < MiniTest::Test
+
+  def setup
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end
+
+  def test_existence
+    assert_instance_of Item, @item1
+  end
+
+  def test_attributes
+    assert_equal "Apple Pie (Slice)", @item2.name
+    assert_equal 2.50, @item2.price
+  end
+
+end
