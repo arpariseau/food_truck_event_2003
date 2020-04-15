130ad5dce09afa0fcd90f22491a02f4e51414375
Alex Pariseau <arpariseau@gmail.com>
2020-04-15 10:32:58 -0600

    Can now get food truck names

--- test/event_test.rb
+++ test/event_test.rb
@@ -38,4 +38,11 @@ class EventTest < MiniTest::Test
     assert_equal [@food_truck1, @food_truck2, @food_truck3], @event.food_trucks
   end
 
+  def test_food_truck_names
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    assert_equal ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @event.food_truck_names
+  end
+
 end
