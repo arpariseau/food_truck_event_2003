26445169196e96c10b2e54f8b9e5daf3a194e548
Alex Pariseau <arpariseau@gmail.com>
2020-04-15 10:28:19 -0600

    Created event class plus setup for tests

--- test/event_test.rb
+++ test/event_test.rb
@@ -1,7 +1,34 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/event'
+require './lib/food_truck'
+require './lib/item'
 
 class EventTest < MiniTest::Test
+
+  def setup
+  @event = Event.new("South Pearl Street Farmers Market")
+  @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+  @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+  @food_truck3 = FoodTruck.new("Palisade Peach Shack")
+  @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+  @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+  @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+  @food_truck1.stock(@item1, 35)
+  @food_truck1.stock(@item2, 7)
+  @food_truck2.stock(@item3, 25)
+  @food_truck2.stock(@item4, 50)
+  @food_truck3.stock(@item1, 65)
+  end
+
+  def test_existence
+    assert_instance_of Event, @event
+  end
+
+  def test_attributes
+    assert_equal "South Pearl Street Farmers Market", @event.name
+    assert_equal [], @event.food_trucks
+  end
 
 end
